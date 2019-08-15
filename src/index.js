6328baea67608448c572d297ad45dc0d298f08ed
Iyara Ferguson <fergusoniyara@gmail.com>
2019-08-15 15:50:33 +0100

    chore(Heroku):Deploy app on heroku This PR creates npm scripts for deploying the app to heroku [Starts #167733809]

--- src/index.js
+++ src/index.js
@@ -28,8 +28,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(require('method-override')());
-
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
