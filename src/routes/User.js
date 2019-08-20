b279d3c155a6b54f10b36c83053e397a10e7c8c9
Nnaemeka Eziamaka <eziamakanv@gmail.com>
2019-08-20 15:58:24 +0100

    delete files added for the feature

--- src/routes/User.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import userController from '../controllers/User';
-
-const router = express.Router();
-
-router.post('/signup', userController.signup);
-
-export default router;
