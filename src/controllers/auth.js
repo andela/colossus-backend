1211d4a870972048d7c21f59fc8a65c83f5d4368
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2019-08-22 11:38:24 +0100

    feature(login): set up sign-in endpoint [Starts #167733813]

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 /* eslint-disable require-jsdoc */
 import jwt from 'jsonwebtoken';
+import { compareSync } from 'bcryptjs';
 import models from '../models';
 
 const UserModel = models.User;
@@ -38,6 +39,39 @@ class AuthController {
           },
         });
       }
+    } catch (error) {
+      return res.status(500).json({ status: 500, error });
+    }
+  }
+
+  static async signIn(req, res) {
+    try {
+      const {
+        email,
+        password,
+      } = req.body;
+      let user = await UserModel.findOne({ where: { email, } });
+      // Check if the user does not exist
+      if (!user) {
+        return res.status(400).json({ status: 400, error: 'Unauthorized', });
+      }
+      user = user.dataValues;
+      // Check if the password matches with the user's password in the db
+      if (!compareSync(password, user.password)) {
+        return res.status(400).json({ status: 400, error: 'Unauthorized', });
+      }
+      // Generate JWT
+      const token = jwt.sign({ id: user.id, email, }, process.env.JWT_SECRET, { expiresIn: '50h' });
+      return res.status(200).json({
+        status: 200,
+        data: {
+          id: user.id,
+          token,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email,
+        },
+      });
     } catch (error) {
       return res.status(500).json({ status: 500, error });
     }
