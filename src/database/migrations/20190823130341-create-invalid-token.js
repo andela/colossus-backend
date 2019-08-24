8abfd324f208fc2c650a18198b6a1162ec4789ff
Kingsley Victor <javaprodigy56@gmail.com>
2019-08-24 05:47:30 +0100

    feat(logout): update user migration file ``  [Finishes #167733816]

--- src/database/migrations/20190823130341-create-invalid-token.js
+++ src/database/migrations/20190823130341-create-invalid-token.js
@@ -1,31 +1,28 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('InvalidTokens', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      actual: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-          notEmpty: {
-            msg: 'Actual token is required'
-          }
+  up: (queryInterface, Sequelize) => queryInterface.createTable('InvalidTokens', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    actual: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Actual token is required'
         }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
-    });
-  },
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
   down: (queryInterface) => queryInterface.dropTable('InvalidTokens')
 };
