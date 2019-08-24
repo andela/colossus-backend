17a890430b04828d086414633200f0d91b00d6be
evansinho <igiri.evanson@gmail.com>
2019-08-24 22:52:06 +0100

    ft(authentication):User can login to the app via facebook and google -create an app on facebook and aquire permission through their credentials -goto google developer console to get the required keys to use the G+ api -install passport-facebook and passport-google -then add their respective strategies to the auth folder -add routes for facebook and google login -[finishes #167733814]

--- src/auth/facebook.js
+++ src/auth/facebook.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import models from '../models';
 import dotenv from 'dotenv';
+import generator from 'generate-password';
 
 dotenv.config();
 
@@ -10,30 +11,32 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 
   passport.use(
     new FacebookStrategy({
-      clientID: process.env.AppID,
+      clientID:process.env.AppID,
       clientSecret:process.env.AppSecret,
       callbackURL:'https://barefoot-nomad.herokuapp.com/api/v1/auth/facebook/callback',
       proxy: true,
       profileFields: ['id', 'emails', 'name']
     }, async (accessToken, refreshToken, profile, done) => {
-         try{
-             const user = await UserModel.findOne({ where: { email:profile.emails[0].value } });
-              if(user) return done(null, user);
-               else {
-                const data = {
-                  firstName: profile.name.givenName,
-                  lastName: profile.name.familyName,
-                  email: profile.emails[0].value,
-                  };
-                 const user = await UserModel.create(data);
-                  if (err) return done(err)
-                     return done(null, user);
-                }
-           }catch(err){
-              console.log(err);
+      try{
+        const user = await UserModel.findOne({ where: { email:profile.emails[0].value } });
+          if(user) return done(null, user);
+           else {
+            const genPassword = generator.generate({ length: 10, numbers: true});
+            const data = {
+              firstName: profile.name.givenName,
+              lastName: profile.name.familyName,
+              email: profile.emails[0].value,
+              password: genPassword
+              };
+            const user = await UserModel.create(data,err);
+              if (err) return done(err)
+                return done(null, user);
             }
-          })
-        );
+        } catch (err){
+            console.log(err);
+          }
+        })
+      );
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -42,7 +45,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
   passport.deserializeUser(async(id, done) => {
     try{
       const user = await UserModel.findOne({ where: { id, } });
-       if(user) return done(null, user);
+       if (user) return done(null, user);
       }catch(err){
         console.log(err);
       }
