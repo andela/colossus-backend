96f99a2815ef3c1c0b62ec37ac075f53582bce0e
evansinho <igiri.evanson@gmail.com>
2019-08-24 14:06:26 +0100

    ft(authentication):User can login to the app via facebook and google -create an app on facebook and aquire permission through their credentials -goto google developer console to get the required keys to use the G+ api -install passport-facebook and passport-google -then add their respective strategies to the auth folder -add routes for facebook and google login -[finishes #167733814]

---
+++ src/auth/google.js
@@ -0,0 +1,51 @@
+import passport from 'passport';
+import models from '../models';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const UserModel = models.User;
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+  passport.use(
+    new GoogleStrategy({
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
+      callbackURL:'https://barefoot-nomad.herokuapp.com/api/v1/auth/google/callback',
+      proxy: true
+    }, async (accessToken, refreshToken, profile, done) => {
+         try{
+           const user = await UserModel.findOne({ where: { email:profile.emails[0].value } });
+             if(user) return done(null, user);
+                else {
+                  const data = {
+                      firstName: profile.name.givenName,
+                      lastName: profile.name.familyName,
+                      email: profile.emails[0].value,
+                      };
+             const user = await UserModel.create(data);
+                  if (err) return done(err)
+                     return done(null, user);    
+                  }                 
+             }catch(err){
+                console.log(err);
+              }
+            })
+          );      
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async(id, done) => {
+    try{
+      const user = await UserModel.findOne({ where: { id, } });
+       if(user) return done(null, user);
+      }catch(err){
+        console.log(err);
+      }
+    });
+
+export default passport;
--- src/routes/api/auth.js
+++ src/routes/api/auth.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import passportFacebook from '../../auth/facebook';
+import passportGoogle from '../../auth/google';
 import { AuthController } from '../../controllers';
 import emailValidation from '../../middlewares/Validations/emailValidation';
 import passwordValidation from '../../middlewares/Validations/passwordValidator';
@@ -14,5 +16,21 @@ router.post('/signin', AuthController.signIn);
 router.post('/sendEmail', emailValidation, AuthController.sendEmail);
 router.post('/resetPassword', passwordValidation, AuthController.resetPassword);
 
+
+// GOOGLE ROUTER
+router.get('/google', passportGoogle.authenticate('google', {scope: ['profile', 'email']}));
+
+router.get('/google/callback', 
+  passportGoogle.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+    res.redirect('/dashboard');
+  });
+
+// FACEBOOK ROUTER
+router.get('/facebook', passportFacebook.authenticate('facebook', {scope: ['email']}));
+
+router.get('/facebook/callback', 
+  passportFacebook.authenticate('facebook', { failureRedirect: '/' }),(req, res) => {
+    res.redirect('/dashboard');
+  });
 
 export default router;
