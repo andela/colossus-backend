069f25a6e9bb8b0664955fdca256372f32858ad3
Kingsley Victor <javaprodigy56@gmail.com>
2019-09-05 22:23:09 +0100

    feat(like): add like controller

---
+++ src/controllers/like.js
@@ -0,0 +1,81 @@
+import models from '../models';
+
+const { Like } = models;
+
+/**
+ * Performs operations intrinsic to the like model
+ * @author Kingsley Victor
+ */
+export default class LikeController {
+  /**
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Promise<void>} likes an accommodation facility
+   */
+  static async like(req, res) {
+    try {
+      const { user, params } = req;
+      const data = await Like.create({
+        likedBy: user.id,
+        accommodationId: params.id
+      });
+      res.status(201).json({
+        status: 201,
+        data
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error
+      });
+    }
+  }
+
+  /**
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Promise<void>} unlikes an accommodation facility
+   */
+  static async unlike(req, res) {
+    try {
+      const { user, params } = req;
+      const deleteCount = await Like.deleteByUserAndAccommodation(
+        user.id,
+        params.id
+      );
+      res.status(200).json({
+        status: 200,
+        data: `${deleteCount} item(s) deleted`
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error
+      });
+    }
+  }
+
+  /**
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {Promise<void>} count number of likes an accommodation facility has
+   */
+  static async count(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await Like.countByAccommodation(id);
+      res.status(200).json({
+        status: 200,
+        data
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        error
+      });
+    }
+  }
+}
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -4,3 +4,4 @@ export const tripStatus = validateTripStatus;
 export { checkToken, checkVerified } from './auth';
 export { blob } from './blob';
 export { checkIfBooked, checkIfOwner } from './accommodation';
+export { checkIfPreviouslyLiked, checkIfUnlikeable } from './like';
