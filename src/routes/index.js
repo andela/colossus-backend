7b38cbcb05bbdc716e84b46d2575421ab84326b8
Kingsley Victor <javaprodigy56@gmail.com>
2019-09-06 16:50:27 +0100

    feat(like): update router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
 const router = Router();
+const apiRoutes = require('./api');
 
-router.use('/api/v1', require('./api'));
+router.use('/api/v1', apiRoutes);
 
 export default router;
