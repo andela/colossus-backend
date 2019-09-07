624165c9a6d41ec0cee7125650e242e362850c81
Iyara Ferguson <fergusoniyara@gmail.com>
2019-09-08 00:18:36 +0100

    feat(edit-request)

--- src/routes/api/request.js
+++ src/routes/api/request.js
@@ -1,13 +1,8 @@
 import { Router } from 'express';
 import { RequestController } from '../../controllers';
-<<<<<<< HEAD
 import { tripStatus as validateTripStatus } from '../../middlewares/index';
 import validateTripRequest from '../../middlewares/validateTripRequest';
 import validateEditRequest from '../../middlewares/editRequestValidation';
-=======
-import validateTripRequest from '../../middlewares/Validations/validateTripRequest';
-import validateRequestEdit from '../../middlewares/editRequestValidation';
->>>>>>> feat(edit-request): modify controller
 
 const router = Router();
 
@@ -18,10 +13,5 @@ router.post('/', validateTripRequest, RequestController.createTrip);
 router.patch('/:id', validateEditRequest, RequestController.editRequest);
 
 router.patch('/:requestId/status', validateTripStatus, RequestController.updateStatus);
-
-<<<<<<< HEAD
-=======
-router.patch('/:id', validateRequestEdit, RequestController.editRequest);
->>>>>>> feat(edit-request): modify controller
 
 export default router;
